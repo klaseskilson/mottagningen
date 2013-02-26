1c002ce88a8eb9c85f1cac1f9e57f060b63179e3
Klas Eskilson <klas.eskilson@gmail.com>
2013-02-26 23:45:15 +0100

    cache check method added to weather_model

--- application/models/weather_model.php
+++ application/models/weather_model.php
@@ -7,6 +7,17 @@ class Weather_model extends CI_Model
 		parent::__construct();
 	}
 
+	function check()
+	{
+		$update_freq = $this->config->item("update_freq");
+		$time = date("Y-m-d H:i:s", (time()-$update_freq));
+		$now = date('Y-m-d H:i:s');
 
-	$this->load->database();
+		$this->db->select("*");
+		$this->db->from("weather");
+		$this->db->where("cachedate > '$time'");
+		$this->db->limit(1);
+
+		return $this->db->count_all_results();
+	}
 }
