8f21114b0c8c54b1d48d254ff6eea44954abbe84
Klas Eskilson <klas.eskilson@gmail.com>
2013-02-26 23:43:54 +0100

    weather settings in config folder

---
+++ application/config/leg_weather.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ |	-----------
+ |	leg_weather.php
+ |	-----------
+ |	Settings for weather collector
+ |
+ |	Klas Eskilson 2013
+*/
+
+
+// set update frequency for weather collecting
+// currently set to 10 min
+$config['update_freq'] = (60*10);
+
