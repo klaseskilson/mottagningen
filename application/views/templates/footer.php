6b71f1f98239e25b87bb3bcd8cc8861e3b9eb143
Klas Eskilson <klas.eskilson@gmail.com>
2013-03-06 20:02:02 +0100

    added google analytics to old footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,17 @@
 <?php // footer ?>
 </body>
+<script type="text/javascript">
+
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-39071997-1']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
+</script>
 </html>
 <!-- Legionen kastade ihop sidan på {elapsed_time} sekunder. BARA FÖR DIG! Man skulle kanske kunna säga att legionen är snäll. -->
