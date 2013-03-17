cd60b4d99f4fe4dc46327e653dde53824defbb6b
Klas Eskilson <klas.eskilson@gmail.com>
2013-03-17 23:21:21 +0100

    moved weather controll to new ctrl controller

---
+++ application/controllers/ctrl.php
@@ -0,0 +1,37 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Ctrl extends CI_Controller
+{
+	/**
+	 * Controller for easy-access controll
+	 */
+
+	public function index()
+	{
+		show_404();
+	}
+
+	public function weather($action = '')
+	{
+		$this->config->load("leg_weather");
+		$this->load->model("Weather_model");
+
+		if($action == 'magic')
+		{
+			var_dump($this->Weather_model->magic());
+		}
+		elseif($action == 'force')
+		{
+			$collect = $this->Weather_model->collect();
+			$run = $this->Weather_model->update($collect['weather'], $collect['temp']);
+			echo ($run ? 'game' : 'no game' );
+		}
+		elseif($action == 'get')
+		{
+			var_dump($this->Weather_model->collectcache());
+		}
+		else{
+			show_404();
+		}
+	}
+}
