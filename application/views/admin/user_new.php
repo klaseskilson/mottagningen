802c74571ac1aafa6d9142e9300239dcb7ed66f1
Klas Eskilson <klas.eskilson@gmail.com>
2013-04-14 23:40:04 +0200

    updating password and creating user works fine now, also privil-check works

---
+++ application/views/admin/user_new.php
@@ -0,0 +1,48 @@
+<div id="container">
+	<div class="row-fluid">
+		<div class="span4">
+			<h4>Skapa användare</h4>
+			<p>Här kan du skapa en ny användare till systemet.</p>
+			<?php
+			if(isset($message))
+			{
+				echo ($message ? '<div class="alert">Användare skapad!</div>' : '<div class="alert alert-error">Gick inte att skapa användaren!</div>');
+			}
+			?>
+		</div>
+		<div class="span4">
+			<form class="form-horizontal" action="/admin/user/new_run" method="post">
+				<h4>&nbsp;</h4>
+				<div class="control-group">
+					<label class="control-label" for="liuid">LiU-id</label>
+					<div class="controls">
+						<input type="text" id="liuid" name="liuid" placeholder="LiU-id" />
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="fname">Förnamn</label>
+					<div class="controls">
+						<input type="text" id="fname" name="fname" placeholder="Förnamn" />
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="sname">Efternamn</label>
+					<div class="controls">
+						<input type="text" id="sname" name="sname" placeholder="Efternamn" />
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="passw">Lösenord</label>
+					<div class="controls">
+						<input type="text" id="passw" name="passw" placeholder="Lösenord" />
+					</div>
+				</div>
+				<div class="control-group">
+					<div class="controls">
+						<button type="submit" class="btn">Skapa!</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
