d1487c44055694754c71f64e3893da1d518e2a6d
Klas Eskilson <klas.eskilson@gmail.com>
2013-06-12 00:29:58 +0200

    Sky+clouds rendering on the go, desert footer. Next to do: sun

--- application/views/new.php
+++ application/views/new.php
@@ -2,6 +2,6 @@
 // new fancy layout
 ?>
 
-<div id="container">
-	<div id="clouds"></div>
+<div id="content" class="wrapper">
+
 </div>
--- application/views/templates/new_footer.php
+++ application/views/templates/new_footer.php
@@ -1,4 +1,14 @@
 <?php // footer ?>
+	<div class="clearfix"></div>
+	<div id="footer">
+		<div class="wrapper">
+			<p>
+				<a href="http://www.yr.no/place/Sweden/%C3%96sterg%C3%B6tland/Norrk%C3%B6ping/" target="_blank">Weather forecast from yr.no, delivered by the Norwegian Meteorological Institute and the NRK</a>
+			</p>
+		</div>
+	</div>
+
+</div><!-- #container -->
 
 <!-- legionens swaga animationer -->
 <script src="/web/js/animate.js" type="text/javascript"></script>
